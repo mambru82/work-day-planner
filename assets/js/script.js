5209b01c3df62c49965e3505330f769b7bea97fb
mambru <ecastro82@gmail.com>
2021-01-09 22:33:40 -0600

    working on date subtraction

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,12 @@
 var loadTasks = function () {
-    tasks = JSON.parse(localStorage.getItem("tasks"));
+    tasks = JSON.parse(localStorage.getItem("day-tasks"));
+
+    if (!tasks) {
+        tasks = [
+            { time: "",
+              task: "" }
+        ]
+    }
 }
 $(".activity-text").on("click", "p", function(){
     var text = $(this)
@@ -12,15 +19,33 @@ $(".activity-text").on("click", "p", function(){
 
     $(this).replaceWith(textInput);
     textInput.trigger("focus");
-    console.log(textInput);
+    // console.log(textInput);
     
     $(".btn").on("click", function () {
-    var newText = $("textarea").val();
+    var newText = $(this).parent().find("textarea").val();
     console.log(newText);
+    // console.log(this);
 
-    var timeValue = $("time-value").text().trim();
-    console.log(timeValue);
+    var timeValue = $(this).parent().children(".time-value").contents().text().trim();
+    ampmCheck = timeValue[timeValue.length-2] + timeValue[timeValue.length-1];
+    console.log(timeValue, ampmCheck);
 
+    var taskP = $("<p>")
+    .text(newText);
+
+    $(this).parent().find("textarea").replaceWith(taskP);
 
     })
 })
+
+var timeCheck = function () {
+    var timeValue = $(".time-9").text().trim();
+    ampmCheck = timeValue[timeValue.length-2] + timeValue[timeValue.length-1];
+    if (ampmCheck === "AM") {
+        timeNumber = timeValue.splice(timeValue.length-2, 2, "");
+    } else {
+        timeNumber = timeValue.splice(timeValue.length-2, 2, "");
+    }
+    console.log(timeNumber, ampmCheck);
+    console.log(timeValue, ampmCheck);
+}
